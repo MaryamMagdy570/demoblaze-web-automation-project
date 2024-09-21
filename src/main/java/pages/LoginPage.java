0784c41037b9964df86bc3cc76aa6c066cbdd88c
BasantMohamed33 <basantm335@gmail.com>
2024-09-21 15:49:37 +0300

    adding loginpage class

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
